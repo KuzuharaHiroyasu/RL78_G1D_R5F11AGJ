c67f22f7beca593340971c7dbf1ebca917c0addb
KuzuharaHiroyasu <h-kuzuhara@one-a.co.jp>
2020-05-20 17:23:27 +0900

    1.0.1.0 (製品版)　生産しなおし

--- rBLE/src/sample_app/user_main_tbl.h
+++ rBLE/src/sample_app/user_main_tbl.h
@@ -38,7 +38,7 @@ const struct ke_state_handler user_main_default_handler = KE_STATE_HANDLER_NONE;
 /* ユーザー定義												*/
 /************************************************************/
 /* バージョン表記の注意事項 */
-const B		version_product_tbl[]= {1, 0, 0, 0};				/* ソフトウェアバージョン */
+const B		version_product_tbl[]= {1, 0, 1, 0};				/* ソフトウェアバージョン */
 																/* バージョン表記ルール */
 																/* ①メジャーバージョン：[0 ～ 99] */
 																/* ②マイナーバージョン：[0 ～ 9] */
